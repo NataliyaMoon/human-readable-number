ba4f1ca9f9f81436b522992bbedf45de478c3d27
Moon Natalia <nataliya.moon@gmail.com>
2024-07-30 15:19:32 +0500

    feat: add solution to the problem

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable(number) {
+    const ones = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine',
+        'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen',
+        'seventeen', 'eighteen', 'nineteen'];
+    const tens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+
+    if (number < 20) {
+        return ones[number];
+    } else if (number < 100) {
+        let remainder = number % 10;
+        return tens[Math.floor(number / 10)] + (remainder ? ' ' + ones[remainder] : '');
+    } else {
+        let remainder = number % 100;
+        let hundreds = Math.floor(number / 100);
+        let tensAndOnes = toReadable(remainder);
+        return ones[hundreds] + ' hundred' + (remainder ? ' ' + tensAndOnes : '');
+    }
 }
